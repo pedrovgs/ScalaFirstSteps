6f1c732a75b291c12ba64d3c21b000a49d05b243
pedrovgs <pedrovicente.gomez@gmail.com>
2014-04-18 19:40:53 +0200

    Create project and add HelloWorldSample. Welcome to Scala!

---
+++ src/HelloWorld.scala
@@ -0,0 +1,15 @@
+/**
+ * First code written in Scala \o/
+ * This file contains an object declaration with a main method that prints a simple Hello Scala World!.
+ *
+ * Take into account how the language use the "object" word to create a singleton object. In scala the usage of
+ * statics is not allowed. Inside the object a main function with an Array[String] is declared using the "def"
+ * word.
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+object HelloWorld {
+  def main(args: Array[String]) {
+    println("Hello Scala World!")
+  }
+}
